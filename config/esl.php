d7ca3dea6997ab9cf4b35f74bd0266ef17185378
Mahmoudalziem <mbdalzym376@gmail.com>
2025-11-29 12:11:42 +0200

    handling freeswitch esl laravel connection

--- config/esl.php
+++ config/esl.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'host' => env('ESL_HOST', '127.0.0.1'),
-    'port' => env('ESL_PORT', 8021),
-    'password' => env('ESL_PASSWORD', 'ClueCon'),
+    'host' => env('FREESWITCH_ESL_HOST', '127.0.0.1'),
+    'port' => env('FREESWITCH_ESL_PORT', 8021),
+    'password' => env('FREESWITCH_ESL_PASSWORD', 'ClueCon'),
 ];
